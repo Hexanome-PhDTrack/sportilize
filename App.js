2fd06ef61d56a6a7920210ab4f6885004d8d1ec8
MartinThibaud <martin.thibaud@dbmail.com>
2022-04-15 08:21:56 +0200

    Added additional dependencies + refactoring App component.

--- App.js
+++ App.js
@@ -3,8 +3,12 @@ import * as eva from '@eva-design/eva';
 import { ApplicationProvider } from '@ui-kitten/components';
 import HomeScreen from './components/HomeScreen'
 
-export default () => (
-  <ApplicationProvider {...eva} theme={eva.light}>
-    <HomeScreen />
-  </ApplicationProvider>
-);
+const App = () => {
+  return (
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <HomeScreen />
+    </ApplicationProvider>
+  )
+}
+
+export default App;
