e5322bc7d016723882ca81ab9c3bcea071b5bcf2
MartinThibaud <martin.thibaud@dbmail.com>
2022-04-27 10:54:06 +0200

    Renamed "shared" folder to "header" folder.

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import * as eva from '@eva-design/eva';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
 import { default as theme } from './custom-theme.json';
-import Header from './components/shared/Header';
+import Header from './components/header/Header';
 import HomeScreen from './components/HomeScreen';
 
 export default () => {
