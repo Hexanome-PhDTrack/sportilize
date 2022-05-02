2784f47e0020cfe9838a618870bf5564ceec7642
Martin Thibaud <martin.thibaud@dbmail.com>
2022-05-02 18:24:46 +0200

    Replace Dahsboard's view with SafeAreaView.

--- Screens/Dashboard.js
+++ Screens/Dashboard.js
@@ -5,13 +5,13 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import Header from "./Header/Header";
 import Footer from "../components/Footer";
+import { SafeAreaView } from "react-native";
 
-import { View } from "react-native";
 const Stack = createStackNavigator();
 const Dashboard = ({ NavigateToScreen, LoggedInUser }) => {
   // put all screens here as Stack.Screen
   return (
-    <View style={{ flex: 1 }}>
+    <SafeAreaView style={{ flex: 1 }}>
       <Header NavigateToScreen={NavigateToScreen} LoggedInUser={LoggedInUser} />
       <NavigationContainer>
         <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName="Map">
@@ -20,7 +20,7 @@ const Dashboard = ({ NavigateToScreen, LoggedInUser }) => {
         </Stack.Navigator>
         <Footer />
       </NavigationContainer>
-    </View>
+    </SafeAreaView>
   );
 };
 
