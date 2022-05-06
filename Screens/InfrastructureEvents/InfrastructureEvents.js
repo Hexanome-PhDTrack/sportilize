d2595cfee8813eb1be6ff51c35e68c40cdce4036
MartinThibaud <martin.thibaud@dbmail.com>
2022-05-06 08:26:28 +0200

    Added screen component.

---
+++ Screens/InfrastructureEvents/InfrastructureEvents.js
@@ -0,0 +1,7 @@
+import React, { useState, useEffect } from 'react';
+import { View } from 'react-native';
+
+export default InfrastructureEvents = ({ route, navigation }) => {
+    const { infrastructure } = route.params;
+    
+}
